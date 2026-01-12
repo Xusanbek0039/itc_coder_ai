0b2f652cc8b97eafab8ff6851e9c229016c74af0
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-01-12 14:47:00 +0500

    Update

--- chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChatView
-
-urlpatterns = [
-    path('chat/', ChatView.as_view(), name='chat_list_create'),
-    path('chat/<int:session_id>/', ChatView.as_view(), name='chat_detail'),
-]
