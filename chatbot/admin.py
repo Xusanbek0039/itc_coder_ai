9ca402303a34e4cee928851a7644d42151329c55
Husanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2026-01-12 14:24:05 +0500

    Update

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ChatSession, ChatMessage
 
-# Register your models here.
+class ChatMessageInline(admin.TabularInline):
+    model = ChatMessage
+    extra = 0
+    readonly_fields = ('created_at',)
+
+@admin.register(ChatSession)
+class ChatSessionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'user')
+    inlines = [ChatMessageInline]
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'session', 'sender', 'created_at')
+    list_filter = ('sender', 'created_at')
